b49b9e44c750f031e70ff5bd12be38b6c7bf1b51
ujjaval01 <sainiujvl@gmail.com>
2024-02-22 18:35:30 +0530

    chamges

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String args[]) {
+        int var1 = 10;
+
+        System.out.println(var1);
+    }
+
+}
--- wrapperClass.java
+++ wrapperClass.java
@@ -2,7 +2,7 @@ public class wrapperClass {
     public static void main(String[] args) {
         int i = 10;
         Integer in = new Integer(i); // boxing and wrapping
-        int j = in.intValue();
+        int j = in.intValue(); // unboxing
         System.out.println(in);
         System.out.println(j);
 
