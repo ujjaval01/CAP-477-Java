f0d2789a3e6a3fdff8e6c5170fb3ff84146e83a3
Ujjaval Saini <sainiujvl@gmail.com>
2025-06-12 15:20:50 +0530

    added more practice program

---
+++ dsaWithJava/string/PrintingAllSubstings.java
@@ -0,0 +1,11 @@
+
+class PrintingAllSubstings{
+    public static void main(String[] args) {
+        String str = "abcde";
+        for(int i = 0; i<= str.length() -1; i++){
+            for(int j = i+1; j<=str.length(); j++){
+                System.out.print(str.substring(i,j) + " ");
+            }
+        }
+    }
+}
