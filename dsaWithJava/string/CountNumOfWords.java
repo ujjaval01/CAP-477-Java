de455b3f1ff4e7bf7841a5aa3b93a506858058f7
Ujjaval Saini <sainiujvl@gmail.com>
2025-06-22 13:01:02 +0530

    more added

--- dsaWithJava/string/CountNumOfWords.java
+++ dsaWithJava/string/CountNumOfWords.java
@@ -1,5 +1,15 @@
 public class CountNumOfWords {
     public static void main(String[] args) {
-        
+        String str = "  u  j ";
+        int ln = str.length();
+        String[] word = str.trim().split("\\s+");
+        // int ln2 = word.length();
+         int count = word.length;
+        System.out.println(str+ ln);
+        // System.out.println(word + ln2);
+
+
+
+         System.out.println("The total number in words \"" + word + "\" is: " + count);
     }
 }
