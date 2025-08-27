430cde4002c5f66cb8b76e1f9655a2b3b45121a1
Ujjaval Saini <sainiujvl@gmail.com>
2025-08-27 23:10:32 +0530

    finally block with exception  & array

--- dsaWithJava/src/ExceptionHandlingPractice/FinallyBlock.java
+++ dsaWithJava/src/ExceptionHandlingPractice/FinallyBlock.java
@@ -4,11 +4,14 @@ public class FinallyBlock {
     static void main() {
         int a = 8, b = 0;
         try{
-            int c = a / b;
-            System.out.println(c);
-        }catch (Exception e){
+           if(b == 0){
+               throw new IllegalArgumentException("I am illegal exception");
+           }
+        } catch(IllegalArgumentException e){
             System.out.println(e);
-        } {
+        } catch (Exception e){
+            System.out.println(e);
+        } finally {
             System.out.println("HII, I am always executed finally block...");
         }
     }
---
+++ dsaWithJava/src/arrays/RemoveDuplicateFromArray.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class RemoveDuplicateFromArray {
+    static void main() {
+        int[] arr = {1,2,3,1,4,2,5,3};
+        int[] cleanArr = {};
+        System.out.println(arr.length);
+        for(int i = 1; i <arr.length; i++){
+
+
+        }
+
+
+
+    }
+}
