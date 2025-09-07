3caedd05d89d1e7d781f4edeb07d6fab3d82c430
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-07 23:35:16 +0530

    update bounded genric

---
+++ dsaWithJava/src/genericPractice/BoundedGeneric.java
@@ -0,0 +1,24 @@
+package genericPractice;
+
+/* Isnme hum kisi bhi genric methods ko boundry me bnd kr skte h, jaise we want this method
+   only support numbers, then we want to bounded this like below example....*/
+
+public class BoundedGeneric {
+    static void main() {
+        display("saini");
+        BoundedGeneric b = new BoundedGeneric();
+//        b.duguna("ujvl");   //getting error due to string asign value
+        b.duguna(55);
+
+
+    }
+
+    static <A> void display(A id){
+        System.out.println(id);
+    }
+    public <E extends Number> void duguna(E id){
+        System.out.println(id);
+
+    }
+
+}
