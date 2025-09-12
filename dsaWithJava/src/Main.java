da50f64abb49b4a7fe2ab6f2c6c634b49a010267
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-12 14:26:26 +0530

    unknown

--- dsaWithJava/src/Main.java
+++ dsaWithJava/src/Main.java
@@ -1,5 +1,21 @@
 public class Main{
     static void main() {
-        System.out.println("Hello World!...");
+       String s = "Greek6".toLowerCase();
+       for(int i = 0; i <s.length(); i++){
+           char ch = s.charAt(i);
+           if(ch >= 'a' && ch<= 'z'){
+
+           }else{
+               if(s.length() == s.charAt(s.length()-1)){
+                   System.out.println(s.length());
+               }else{
+                   System.out.println("kuch b");
+               }
+           }
+
+
+       }
+
+
     }
 }
