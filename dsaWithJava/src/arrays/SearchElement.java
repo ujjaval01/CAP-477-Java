958350e58d152caba3f78e6956220a21e433db4c
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-14 01:24:08 +0530

    array practice linear searching(minor fix)

--- dsaWithJava/src/arrays/SearchElement.java
+++ dsaWithJava/src/arrays/SearchElement.java
@@ -3,15 +3,17 @@ package arrays;
 public class SearchElement {
     static void main() {
         int[] arr = {10,52,7,5,77,3,65,52,48,96};
-        Boolean flag = false;
-        int target = 52;
+        boolean flag = false;
+        int target = 96;
+        int indexOf = -1;
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == target){
                 flag = true;
+                indexOf = i;
                 break;
             }
         }
-        if(flag == true) System.out.println("Target exists");
+        if(flag) System.out.println("Target exists at the index of " + indexOf);
         else System.out.println("Target not found");
     }
 }
