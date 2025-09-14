3ac00eece9cabea0067a78be2e3f32aab05bb8ee
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-14 16:41:24 +0530

    array practice(SecondLargestElement)

---
+++ dsaWithJava/src/arrays/MultiplyEachElement.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class MultiplyEachElement {
+    static void main() {
+        int[] arr = {8,4,9,5,9,6, 8,1,3,7};
+//        for (int i = 0; i< arr.length; i++){
+//            System.out.println(arr[i] * 2 + " ");
+//        }
+
+        for(int num:arr){
+            System.out.println(num*2 + " ");
+        }
+    }
+}
---
+++ dsaWithJava/src/arrays/SecondLargestElement3.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SecondLargestElement3 {
+    static void main(String[] args) {
+        int[] arr = {8,4,9,5,9,6, 8,1,3,7};
+                 // 1 3 4 5 6 7 8 8 9 9
+        int n = arr.length-1;
+        Arrays.sort(arr);
+        for(int i = 0; i <= n; i++){
+            if(arr[i] != arr[n]){
+
+            }
+        }
+
+    }
+}
