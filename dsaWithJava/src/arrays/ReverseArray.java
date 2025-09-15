37970c5f9ad4f985be66bbd339242c467a93d1de
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-15 11:41:15 +0530

    array practice(rev array)

--- dsaWithJava/src/arrays/ReverseArray.java
+++ dsaWithJava/src/arrays/ReverseArray.java
@@ -1,5 +1,20 @@
 package arrays;
 
 public class ReverseArray {
+    static void main() {
+        int[] arr = {10,52,7,5,77,3,65,52,48,96};
+        int i = 0;
+        int j = arr.length-1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int n: arr){
+            System.out.print(n + " ");
+        }
+    }
 
 }
