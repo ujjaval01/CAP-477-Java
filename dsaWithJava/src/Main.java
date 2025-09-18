50f0de9a8a986edb22a0b8df20c8c57bf95a27ba
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-18 21:25:28 +0530

    array practice(AddTwoNumRespectByTwoArray)

--- dsaWithJava/src/Main.java
+++ dsaWithJava/src/Main.java
@@ -1,21 +1,7 @@
 public class Main{
     static void main() {
-       String s = "Greek6".toLowerCase();
-       for(int i = 0; i <s.length(); i++){
-           char ch = s.charAt(i);
-           if(ch >= 'a' && ch<= 'z'){
-
-           }else{
-               if(s.length() == s.charAt(s.length()-1)){
-                   System.out.println(s.length());
-               }else{
-                   System.out.println("kuch b");
-               }
-           }
-
-
-       }
-
-
+        int a = 9;
+        int b = 10;
+        System.out.println(a/b);
     }
 }
--- dsaWithJava/src/arrays/Wave.java
+++ dsaWithJava/src/arrays/Wave.java
@@ -1,4 +1,17 @@
 package arrays;
 
 public class Wave {
+    static void main() {
+        int[] arr = {2,4,7,8,9,10};    // 2 1 4 3 5
+        for(int i = 1; i < arr.length; i++){
+            int temp = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = temp;
+            i++;
+
+        }
+        for(int a:arr){
+            System.out.print(a +" ");
+        }
+    }
 }
