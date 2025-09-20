8b3b61e5fe63cbd20f6d741f8014a26e214c4465
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-20 22:27:01 +0530

    RemoveDuplicateFromSortedArray(optimized)

--- dsaWithJava/src/arrays/mergeTwoSortedArray.java
+++ dsaWithJava/src/arrays/mergeTwoSortedArray.java
@@ -27,8 +27,20 @@ public class mergeTwoSortedArray {
                 j++;
                 k++;
             }
-            while()
-
+        }
+        if(i==a.length){
+            while (j< b.length){
+                c[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+        if(j==b.length){
+            while (i< a.length){
+                c[k] = a[i];
+                i++;
+                k++;
+            }
         }
 
     }
