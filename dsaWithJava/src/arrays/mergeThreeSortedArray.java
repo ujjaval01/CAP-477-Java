db173383334a8a4306c44223bf370de3c1037960
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-21 23:23:37 +0530

    mergeThreeSortedArray(optimized)

--- dsaWithJava/src/arrays/mergeThreeSortedArray.java
+++ dsaWithJava/src/arrays/mergeThreeSortedArray.java
@@ -1,38 +1,46 @@
 package arrays;
 
 public class mergeThreeSortedArray {
-    static void main() {
+    public static void main(String[] args) {
         int[] a = {2,4,6,8,9};
         int[] b = {7,11,12,15,15,17};
         int[] c = {1,3,5,7,10};
-        int[] d = new int[a.length+ b.length+ c.length];
-        sortedArray(a,b,c,d);
+        int[] d = new int[a.length + b.length + c.length];
+        sortedArray(a, b, c, d);
         for (int ele: d){
-            System.out.println(ele + " ");
+            System.out.print(ele + " ");
         }
-
     }
 
     private static void sortedArray(int[] a, int[] b, int[] c, int[] d) {
-        int i = 0, j = 0, k= 0, l = 0;
-        while(i<a.length && j<b.length && k<c.length){
-            if(a[i] < b[j] && a[i]< c[k]){
-                d[l] = a[i];
-                i++;
-                l++;
+        int i = 0, j = 0, k = 0, l = 0;
+        while (i < a.length && j < b.length && k < c.length) {
+            if (a[i] < b[j] && a[i] < c[k]) {
+                d[l++] = a[i++];
             } else if (b[j] < a[i] && b[j] < c[k]) {
-                d[l] = b[j];
-                j++;
-                k++;
-            }else{
-                d[l] = c[k];
-                k++;
-                l++;
+                d[l++] = b[j++];
+            } else {
+                d[l++] = c[k++];
             }
         }
-        while (i==a.length && j != b.length){
 
-
+        // agar koi array khatam ho gaya to baaki do ko merge karo
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) d[l++] = a[i++];
+            else d[l++] = b[j++];
         }
+        while (i < a.length && k < c.length) {
+            if (a[i] < c[k]) d[l++] = a[i++];
+            else d[l++] = c[k++];
+        }
+        while (j < b.length && k < c.length) {
+            if (b[j] < c[k]) d[l++] = b[j++];
+            else d[l++] = c[k++];
+        }
+
+        // leftover
+        while (i < a.length) d[l++] = a[i++];
+        while (j < b.length) d[l++] = b[j++];
+        while (k < c.length) d[l++] = c[k++];
     }
 }
