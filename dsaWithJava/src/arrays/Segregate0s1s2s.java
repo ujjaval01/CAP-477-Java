560b6458d6dc53f0bc2258c224f1cce9e054a65d
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-22 22:20:32 +0530

    Segregate0s1s2s3s(optimized and gpt solution)

--- dsaWithJava/src/arrays/Segregate0s1s2s.java
+++ dsaWithJava/src/arrays/Segregate0s1s2s.java
@@ -30,3 +30,45 @@ public class Segregate0s1s2s {
 
     }
 }
+//import java.util.*;
+//
+//public class Main {
+//    public static void sort012(int[] arr) {
+//        int low = 0, mid = 0, high = arr.length - 1;
+//
+//        while (mid <= high) {
+//            switch (arr[mid]) {
+//                case 0:
+//                    swap(arr, low, mid);
+//                    low++;
+//                    mid++;
+//                    break;
+//                case 1:
+//                    mid++;
+//                    break;
+//                case 2:
+//                    swap(arr, mid, high);
+//                    high--;
+//                    break;
+//            }
+//        }
+//    }
+//
+//    private static void swap(int[] arr, int i, int j) {
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr1 = {0, 1, 2, 0, 1, 2};
+//        sort012(arr1);
+//        System.out.println(Arrays.toString(arr1)); // [0, 0, 1, 1, 2, 2]
+//
+//        int[] arr2 = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+//        sort012(arr2);
+//        System.out.println(Arrays.toString(arr2)); // [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+//    }
+//}
+
+
