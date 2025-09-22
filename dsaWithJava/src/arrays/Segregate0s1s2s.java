3a79784b6ee72f1afebc99d7e71fd559e1ab2f7e
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-22 22:10:02 +0530

    Segregate0s1s2s

---
+++ dsaWithJava/src/arrays/Segregate0s1s2s.java
@@ -0,0 +1,32 @@
+package arrays;
+
+public class Segregate0s1s2s {
+
+    // two pass solution
+
+    static void main(String[] args) {
+        int[] arr = {1,1,0,1,0,2,0,2,2,2};
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        for (int a : arr) {
+            if (a == 0) zeros++;
+            else if (a == 1)  ones++;
+            else twos++;
+        }
+        System.out.println(zeros + " " + ones + " " + twos);
+        for (int i = 0; i < zeros; i++){
+            arr[i] = 0;
+        }
+        for(int i = zeros; i< ones; i++){
+            arr[i] = 1;
+        }
+        for(int i = ones; i< arr.length; i++){
+            arr[i] = 2;
+        }
+        for (int b : arr) {
+            System.out.print(b + " ");
+        }
+
+    }
+}
