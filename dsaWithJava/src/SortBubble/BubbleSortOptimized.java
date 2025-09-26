a4308bbfe24bbe6331e9af413db8a37a6aa66518
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-26 22:31:46 +0530

    practice of bubble sorting

--- dsaWithJava/src/SortBubble/BubbleSortOptimized.java
+++ dsaWithJava/src/SortBubble/BubbleSortOptimized.java
@@ -2,24 +2,26 @@ package SortBubble;
 
 public class BubbleSortOptimized {
     static void main(String[] args) {
-        int[] arr = {4,5,8,3,1,2};
-        sortArray(arr);
-        for(int e: arr){
-            System.out.print(e + " ");
-        }
+
+        sortArray();
+
 
     }
 
-    static void sortArray(int[] arr){
+    static void sortArray(){
+        int[] arr = {4,5,8,3,1,2};
         int n = arr.length;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n-1; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
-            }
+       for(int i = 0; i< n-1; i++){
+           for(int j = 0; j< n-i-1; j++){
+               if(arr[j] > arr[i]){
+                   int temp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = temp;
+               }
+           }
+       }
+        for(int e: arr){
+            System.out.print(e + " ");
         }
 
     }
