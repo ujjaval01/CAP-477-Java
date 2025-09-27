119482d0e81c58c1a297ac277b0783f4a5b6ecd5
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-27 18:58:50 +0530

    optimized code of bubble sort

--- dsaWithJava/src/SortBubble/BubbleSortOptimized.java
+++ dsaWithJava/src/SortBubble/BubbleSortOptimized.java
@@ -2,27 +2,31 @@ package SortBubble;
 
 public class BubbleSortOptimized {
     static void main(String[] args) {
-
-        sortArray();
-
-
-    }
-
-    static void sortArray(){
         int[] arr = {4,5,8,3,1,2};
         int n = arr.length;
-       for(int i = 0; i< n-1; i++){
-           for(int j = 0; j< n-i-1; j++){
-               if(arr[j] > arr[i]){
-                   int temp = arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = temp;
-               }
-           }
-       }
+        sortArray(arr,n);
         for(int e: arr){
             System.out.print(e + " ");
         }
+    }
+
+
+    static void sortArray(int[] arr, int n){
+
+       for(int i = 0; i< n-1; i++){
+           boolean flag = false;
+           for(int j = 0; j< n-i-1; j++){
+               if(arr[j] > arr[j+1]){
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+                   flag = true;
+               }
+           }
+           if(!flag){
+               break;
+           }
+       }
 
     }
 }
