a18de6793813182e1d5b4ace8d59a952fd3ad8c4
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-01 18:17:11 +0530

    practice of selection sort

---
+++ dsaWithJava/src/SortSelection/selectionSortWithLstIndx.java
@@ -0,0 +1,27 @@
+package SortSelection;
+
+public class selectionSortWithLstIndx {
+
+    static void main() {
+        int[] arr = {2,4,1,5,3,7,6,9,8};
+        sort(arr);
+        for(int e: arr){
+            System.out.print(e + " ");
+        }
+    }
+    public static void sort(int[] arr){
+        int n = arr.length;
+        for(int i = n-1; i >= 0; i--){
+            int maxIndex = i;
+            for(int j = i+1; j<n-1; j++){
+                if(arr[j] > arr[maxIndex]){
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+
+        }
+    }
+}
