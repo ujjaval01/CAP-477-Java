ea60016d8d8844f3f43ef10b6c7596b7dbed4b2c
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-03 21:08:29 +0530

    optimized two sum

--- dsaWithJava/src/arrays/TwoSum.java
+++ dsaWithJava/src/arrays/TwoSum.java
@@ -4,12 +4,20 @@ public class TwoSum {
     static void main() {
         int[] arr = {1,5,8,-3};
         int targetSum = 2;
-        for (int i = 0; i< arr.length; i++){
-            for(int j = i+1; j<arr.length; j++){
-                if(arr[i] + arr[j] == targetSum){
-                    System.out.println("Sum found:- " + arr[i] + " + " + arr[j] + " = " + targetSum);
-                }
+        int i = 0, j = arr.length-1;
+        for(i = 0; i<arr.length; i++){
+            if(arr[i] + arr[j] != targetSum){
+
             }
         }
+
+
+//        for (int i = 0; i< arr.length; i++){
+//            for(int j = i+1; j<arr.length; j++){
+//                if(arr[i] + arr[j] == targetSum){
+//                    System.out.println("Sum found:- " + arr[i] + " + " + arr[j] + " = " + targetSum);
+//                }
+//            }
+//        }
     }
 }
