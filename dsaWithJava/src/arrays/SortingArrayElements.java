0a21c933b9a260fdb255d454ef6141f1081d20ae
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-04 21:13:39 +0530

    optimized codeof sorting arrays elements

--- dsaWithJava/src/arrays/SortingArrayElements.java
+++ dsaWithJava/src/arrays/SortingArrayElements.java
@@ -5,10 +5,18 @@ import java.util.Arrays;
 
 public class SortingArrayElements {
     public static void main(String[] args) {
-        int a[] = {50,40,90,70,10};
-        for (int i = 0; i<a.length; i++){
-//            System.out.println(a[i]);
-
+        int[] arr = {50,40,90,70,10};
+        for (int i = 0; i<arr.length; i++){
+            for(int j = 1; j< arr.length; j++){
+                if(arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for(int e: arr){
+            System.out.print(e + " ");
         }
     }
 }
