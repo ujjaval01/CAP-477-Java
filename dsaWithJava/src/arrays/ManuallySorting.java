51fe337dedf7de66c85b3e8b214ce301f404dc42
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-05 21:08:28 +0530

    optimized codeof sorting arrays elements

--- dsaWithJava/src/arrays/ManuallySorting.java
+++ dsaWithJava/src/arrays/ManuallySorting.java
@@ -6,12 +6,7 @@ public class ManuallySorting {
         int l = arr.length;
         for(int i = 0; i< l-1; i++){
             for (int j = 0; j < l - 1- i; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]= arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
+
 
         }
         for(int num: arr){
