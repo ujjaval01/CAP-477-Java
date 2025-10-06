3545b872934ba699aa610a78e0217efb212115d9
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-06 08:37:27 +0530

    CHECK MAX ELEMENTS.

--- dsaWithJava/src/arrays/BasicOne.java
+++ dsaWithJava/src/arrays/BasicOne.java
@@ -1,21 +1,38 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class BasicOne {
     public static void main(String[] args) {
-        String[][] name = {{"Ujvl","Vnsi" ,"Aman", "Aadi"},
-                           {"Ujvl","Vnsi" ,"Aman", "Aadi"}};
-//        for (String[] strings : name) {
-//            for (String string : strings) {
-//                System.out.print(string + " ");
+//        String[][] name = {{"Ujvl","Vnsi" ,"Aman", "Aadi"},
+//                           {"Ujvl","Vnsi" ,"Aman", "Aadi"}};
+////        for (String[] strings : name) {
+////            for (String string : strings) {
+////                System.out.print(string + " ");
+////            }
+////            System.out.println();
+////        }
+//
+//        for(int i = 0; i< name.length; i++){
+//            for (int j = 0; j<name[i].length; j++){
+//                System.out.print(name[i][j] + " ");
 //            }
 //            System.out.println();
 //        }
 
-        for(int i = 0; i< name.length; i++){
-            for (int j = 0; j<name[i].length; j++){
-                System.out.print(name[i][j] + " ");
-            }
-            System.out.println();
-        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(20);
+        arr.add(10);
+        arr.add(30);
+        arr.add(10);
+        int i = 0;
+        arr.remove(i);
+        System.out.println(arr);
+
+
+
+
     }
 }
--- dsaWithJava/src/arrays/MaxElement.java
+++ dsaWithJava/src/arrays/MaxElement.java
@@ -3,7 +3,7 @@ package arrays;
 public class MaxElement {
     static void main() {
         int[] arr = {5,8,4,7,9,11,2,3};
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for(int e: arr){
             if (e > max){
                 max = e;
