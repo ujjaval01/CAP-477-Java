908cb7bb2d4ca66e40276d35bf12d55d526ff63a
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-07 16:01:01 +0530

    finding kth largest element using bubble sort

---
+++ dsaWithJava/src/SortBubble/KthLargestElement.java
@@ -0,0 +1,25 @@
+package SortBubble;
+
+public class KthLargestElement {
+    static void main() {
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        // 3 4 7 10 15 20
+        int k = 2;
+        int res = findElement(arr,k);
+        System.out.println(res);
+
+    }
+
+    private static int findElement(int[] arr, int k) {
+        for(int i = 0; i<k; i++){
+            for(int j = 0; j< arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr[arr.length-k];
+    }
+}
