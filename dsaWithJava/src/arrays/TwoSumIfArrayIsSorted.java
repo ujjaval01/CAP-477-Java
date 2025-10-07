6fa9dc7e34d4134f1e241719ae60559cf398e4e3
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-07 10:54:05 +0530

    two sum if array is sorted

--- dsaWithJava/src/arrays/TwoSumIfArrayIsSorted.java
+++ dsaWithJava/src/arrays/TwoSumIfArrayIsSorted.java
@@ -1,28 +1,26 @@
 package arrays;
 
-import java.util.EnumSet;
-
 public class TwoSumIfArrayIsSorted {
-    static void main() {
-        int[] numbers = {2,11,6,7,15};
+    public static void main(String[] args) {  // ✅ correct signature
+        int[] numbers = {2,7,11,15};
         int target = 9;
-        int[] nums = twoSum(numbers,target);
-        System.out.print("Target sum found on ");
+        int[] nums = twoSum(numbers, target);
+
         for(int e: nums){
-            System.out.print("Index " + e + ", ");
+            System.out.print(e + " ");
         }
-
-
-
     }
+
     public static int[] twoSum(int[] numbers, int target) {
-        int i = 0, j = numbers.length-1;
-        while(i<j){
-            if(numbers[i] + numbers[j] == target) return new int[] {i+1, j+1};
-            else if(numbers[i] + numbers[j] > target) j--;
-            else  if(numbers[i] + numbers[j] < target) i++;
+        int i = 0, j = numbers.length - 1;
+
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) return new int[] {i + 1, j + 1};
+            else if (sum > target) j--;
+            else i++;
         }
+
         return new int[] {};
     }
-
 }
