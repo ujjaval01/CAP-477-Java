41da1f03511a23ffe0e13490b1b7ba7d8f73b6a2
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-09 22:08:40 +0530

    finding square root using binary search algo

--- dsaWithJava/src/SearchBinary/SquareRoot.java
+++ dsaWithJava/src/SearchBinary/SquareRoot.java
@@ -16,13 +16,31 @@ public class SquareRoot {
         int res = find(n);
         System.out.println(res);
     }
+
+    // using binary search algo
     static int find(int n){
-        int root = 0;
-        for(int i = 1; i<n; i++) {
-            if (i * i > n) break;
-            root = i;
+        long low = 1, high = n;
+        while(low<=high){
+            long mid = (high + low) / 2;
+            if(mid * mid == n) return (int)mid;
+            else if (mid*mid > n) high = mid -1;
+            else low = mid + 1;
         }
-        return root;
+        return (int) high;
     }
+
+
+
+
+
+    // simple method...
+//    static int find(int n){
+//        int root = 0;
+//        for(int i = 1; i<n; i++) {
+//            if (i * i > n) break;
+//            root = i;
+//        }
+//        return root;
+//    }
 
 }
