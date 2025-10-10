e950432d2087b2fa65afcde1b5d37a05cb59fe88
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-10 23:30:11 +0530

    finding element, who appear only once in a array

--- dsaWithJava/src/SearchBinary/FindOnce.java
+++ dsaWithJava/src/SearchBinary/FindOnce.java
@@ -13,7 +13,8 @@ Output: 1
 
 public class FindOnce {
     static void main() {
-        int[] arr = {1,1,2,2,3,4,4,5,5};
+//        int[] arr = {1,1,2,2,3,4,4,5,5};
+        int[] arr = {1, 2, 2, 3, 3};
         int res = findOnce(arr);
         System.out.println(res);
     }
@@ -22,12 +23,25 @@ public class FindOnce {
         int n = arr.length;
         int low = 0;
         int high = n-1;
+        if(n == 1) return arr[0];
+        if(arr[0] != arr[1]) return arr[0];
+        if(arr[n-1] != arr[n-1]) return arr[n-1];
         while(low <= high){
             int mid = low + (high - low) / 2;
             if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
-            if(arr[mid] )
-
-
+            int fst = mid, snd = mid;
+            if(arr[mid-1] ==  arr[mid]){
+                fst = mid-1;
+            }else{
+                snd = mid+1;
+            }
+            int leftCount = fst - low;
+            int rightCount = high - snd;
+            if(leftCount % 2 == 0){
+                low = snd + 1;
+            }else {
+                high = fst -1;
+            }
         }
         return 123;
     }
