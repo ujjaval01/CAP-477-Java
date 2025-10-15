6ae9bb65b6e9d567afe95fee3a06d0ecd99c286c
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-15 09:52:58 +0530

    leetcode qstn 231 square of two number

---
+++ dsaWithJava/src/LeetCodeQuestions/Q231SquareOfTwo.java
@@ -0,0 +1,24 @@
+package LeetCodeQuestions;
+
+public class Q231SquareOfTwo {
+    public static void main(String[] args){
+        int n = 64;
+        boolean b = check(n);
+        System.out.println(b);
+
+    }
+    static boolean check(int n){
+
+
+
+//        if(n == 0) return true;
+//        if(n == 2) return true;
+//        if(n%2 != 0) return false;
+//        for(int i = 2; i<= n; i*=2){  //  i = 2,4,8,16
+//            int j = i*2; //               j = 4,8,16
+//                           //           i = i*2-> 2*2 = 4, 4*2 = 8, 8*2 = 16
+//            if(j==n) return true;//       4, 8 , 16,
+//        }
+//        return false;
+    }
+}
