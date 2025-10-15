e611f22f9b2066d9c6b1299384df4eb8624fa209
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-15 19:08:50 +0530

    searching element in a sorted 2D matrix using binary search(question added)

--- dsaWithJava/src/SearchBinary/searchAnElementInSortedMatrix.java
+++ dsaWithJava/src/SearchBinary/searchAnElementInSortedMatrix.java
@@ -1,5 +1,6 @@
 package SearchBinary;
-/*Given a strictly sorted 2D matrix mat[][] of size n x m and a number x. Find whether the number x is present in the matrix or not.
+/* LC - 74. Search a 2D Matrix
+Given a strictly sorted 2D matrix mat[][] of size n x m and a number x. Find whether the number x is present in the matrix or not.
 Note: In a strictly sorted matrix, each row is sorted in strictly increasing order, and the first element of the ith row (i!=0) is
 greater than the last element of the (i-1)th row.
 Examples:
