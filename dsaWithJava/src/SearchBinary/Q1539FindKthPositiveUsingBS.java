d5e4d711d7feebedc163fe2eca6b3fe7026b7e3f
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-19 11:39:57 +0530

    solving leetcode qstn 153 using binnary search kth missing positive number in sorted array

---
+++ dsaWithJava/src/SearchBinary/Q1539FindKthPositiveUsingBS.java
@@ -0,0 +1,25 @@
+package SearchBinary;
+
+public class Q1539FindKthPositiveUsingBS {
+    static void main() {
+        int[] arr = {2,3,5,6,8,9};
+        int k = 3;
+        int res = find(arr, k);
+        System.out.println(res);
+    }
+    static int find(int[] arr, int k){
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            int currValue = mid + 1;
+            int missing = arr[mid] - currValue;
+            if(missing >= k){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low + k;
+    }
+}
