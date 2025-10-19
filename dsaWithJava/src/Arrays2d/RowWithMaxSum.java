03e8bd61fb0ba963f3356891e35cdb3408173508
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-19 15:44:23 +0530

    practice of 2d array(finding and return row with max sum)(optimized)

--- dsaWithJava/src/Arrays2d/RowWithMaxSum.java
+++ dsaWithJava/src/Arrays2d/RowWithMaxSum.java
@@ -2,20 +2,21 @@ package Arrays2d;
 
 public class RowWithMaxSum {
     static void main() {
-        int[][] arr = {{4, 5, 6}, {7, 1, 8}, {9, 2, 1}};
+        int[][] arr = {
+                {100, -90, -90},  // total sum = -80
+                {5, 5, 5}         // total sum = 15
+        };
         int maxSum = Integer.MIN_VALUE;
-        int rowSum = 0;
-        int row = 0;
+        int row = -1;
+
         for(int i = 0; i< arr.length; i++){
+            int rowSum = 0;
             for(int j = 0; j<arr[0].length; j++){
-                if(j == 0){
-                    rowSum = 0;
-                }
                 rowSum = rowSum + arr[i][j];
-                if(maxSum < rowSum){
-                    maxSum = rowSum;
-                    row = i;
-                }
+            }
+            if(maxSum < rowSum){
+                maxSum = rowSum;
+                row = i;
             }
         }
         System.out.println(maxSum + " is the maxSum, Present in the row " + row);
