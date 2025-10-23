4e0d00c47ff1338d840651074bbb0f31f6b96478
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-23 21:35:53 +0530

    leetcode qstn 54 printing spiral matrix

--- dsaWithJava/src/LeetCodeQuestions/Q54SpiralMatrix.java
+++ dsaWithJava/src/LeetCodeQuestions/Q54SpiralMatrix.java
@@ -1,4 +1,23 @@
 package LeetCodeQuestions;
+/*You are given a rectangular matrix mat[][] of size n x m,
+ and your task is to return an array while traversing the matrix in spiral form.
+Examples:
+Input: mat[][] = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+Output: [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+
+  1 ---> 2 ---> 3 --->  4       (1->2->3->4->8->12->16->15->14->13->9->5->6->7->11->10)
+                        |
+  5 ---> 6 ---> 7       8       ------------------------->
+  |             |       |      | --------------------->   |
+  9     10 <--- 11     12      |   | --------------->  |  |
+  |                     |      |   | <--------------   |  |
+  13 <-- 14 <-- 15 <-- 16      |   <-------------------|  |
+                               <--------------------------
+  */
+
+
+
+
 
 import java.util.ArrayList;
 import java.util.List;
