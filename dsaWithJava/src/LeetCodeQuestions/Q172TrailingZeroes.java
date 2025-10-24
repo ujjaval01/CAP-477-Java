3e68f430488283dc55ff86e9f3f4ff5f10460123
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-24 20:11:44 +0530

    lc quesion tratrailingZeroes

--- dsaWithJava/src/LeetCodeQuestions/Q172TrailingZeroes.java
+++ dsaWithJava/src/LeetCodeQuestions/Q172TrailingZeroes.java
@@ -1,21 +1,17 @@
 package LeetCodeQuestions;
 
-public class Q172TrealingZeros {
+public class Q172TrailingZeroes {
     static void main() {
         int n = 125;
         int ans = trailingZeroes(n);
         System.out.println(ans);
-
-
     }
     static int trailingZeroes(int n) {
        int count = 0;
        while(n>0){
-           n = n/5;
-           count = count + n;
+           n/=5;
+           count += n;
        }
-
        return count;
     }
-
 }
