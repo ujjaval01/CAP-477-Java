63b9c45e80525e97d5bbbc381986459774bf3628
Ujjaval Saini <sainiujvl@gmail.com>
2025-10-24 22:42:30 +0530

    factorial using recursion

---
+++ dsaWithJava/src/Recursion/factorial.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class factorial {
+    static void main() {
+        int n = 8;
+        int ans = find(n);
+        System.out.println(ans);
+    }
+    static int find(int n){
+//        if(n == 0 || n == 1) return 1;
+//        return n * find(n-1);
+        return (n<=1) ? 1 : n*find(n-1);
+    }
+}
