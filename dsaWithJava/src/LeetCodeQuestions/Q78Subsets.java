708ad89aed7226032a8e8984c944c4182cc5c632
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-03 09:12:53 +0530

    added two gfg question of recursion(checking subsets of a string)(sum of all subsets in the list)

---
+++ dsaWithJava/src/LeetCodeQuestions/Q78Subsets.java
@@ -0,0 +1,34 @@
+package LeetCodeQuestions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Q78Subsets {
+    static void main() {
+        int[] nums = {1, 2, 3};
+        System.out.println(subsets(nums));
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(new ArrayList<>(), nums, 0, list);
+        // Collections.sort() list of lists पर काम नहीं करेगा सीधे, इसलिए skip कर रहे हैं
+        return list;
+    }
+
+    public static void helper(List<Integer> ans, int[] nums, int idx, List<List<Integer>> list) {
+        if (idx == nums.length) {
+            list.add(new ArrayList<>(ans));
+            return;
+        }
+
+        // include current element
+        ans.add(nums[idx]);
+        helper(ans, nums, idx + 1, list);
+
+        // exclude current element
+        ans.remove(ans.size() - 1);
+        helper(ans, nums, idx + 1, list);
+    }
+}
---
+++ dsaWithJava/src/LeetCodeQuestions/gfgSubsetSum.java
@@ -0,0 +1,35 @@
+package LeetCodeQuestions;
+/*Given a array arr of integers, return the sums of all subsets in the list.  Return the sums in any order.
+Examples:
+Input: arr[] = [2, 3]
+Output: [0, 2, 3, 5]
+Explanation: When no elements are taken then Sum = 0. When only 2 is taken then Sum = 2. When only 3 is taken then Sum = 3. When elements 2 and 3 are taken then Sum = 2+3 = 5.
+Input: arr[] = [1, 2, 1]
+Output: [0, 1, 1, 2, 2, 3, 3, 4]
+Explanation: The possible subset sums are 0 (no elements), 1 (either of the 1's), 2 (the element 2), and their combinations.*/
+import java.util.ArrayList;
+
+public class gfgSubsetSum {
+    static void main() {
+        int[] arr = {2,3};
+        System.out.println(subsetSums(arr));
+    }
+    public static ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        helper(arr, 0, 0, list);
+        // return (ArrayList<Integer>) list;  // typecasting, if we are using list instead of arrayList
+        return list;
+    }
+    public static void helper(int[] arr, int idx, int sum, ArrayList<Integer> list){
+        if(arr.length == idx){
+            list.add(sum);
+            return;
+        }
+
+        helper(arr, idx+1, sum+arr[idx], list);
+        helper(arr, idx+1, sum, list);
+
+    }
+}
