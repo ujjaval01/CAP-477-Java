7b29b4e9e85e9710e0bf13fe7f55d4157a45a15d
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-04 21:53:59 +0530

    added leetcode simple question of string checking first occurrence(optimized, using inbuilt function)

--- dsaWithJava/src/LeetCodeQuestions/Q28FindTheIndexOfTheFirstOccurrenceInString.java
+++ dsaWithJava/src/LeetCodeQuestions/Q28FindTheIndexOfTheFirstOccurrenceInString.java
@@ -8,18 +8,19 @@ public class Q28FindTheIndexOfTheFirstOccurrenceInString {
 
     }
     public static int checkFirstOcc(String s, String t){
-        int i = 0, j = 0;
-        while(i<s.length()){
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
-                j++;
-                if(j == t.length()) return i - j;
-            }
-            else{
-                i = i - j + 1;
-                j = 0;
-            }
-        }
-        return -1;
+        return s.indexOf(t);
+//        int i = 0, j = 0;
+//        while(i<s.length()){
+//            if(s.charAt(i) == t.charAt(j)){
+//                i++;
+//                j++;
+//                if(j == t.length()) return i - j;
+//            }
+//            else{
+//                i = i - j + 1;
+//                j = 0;
+//            }
+//        }
+//        return -1;
     }
 }
