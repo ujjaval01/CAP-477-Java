2301b7b9553128d8a0e67408ae14c26fb5f0d785
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-12 21:30:58 +0530

    gfg question to check inversion count in array using merge sort(also added question)

--- dsaWithJava/src/sortMerge/inversionCount.java
+++ dsaWithJava/src/sortMerge/inversionCount.java
@@ -1,5 +1,16 @@
 package sortMerge;
-
+/*Given an array of integers arr[]. You have to find the Inversion Count of the array.
+Note : Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+Examples:
+Input: arr[] = [2, 4, 1, 3, 5]
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+Input: arr[] = [2, 3, 4, 5, 6]
+Output: 0
+Explanation: As the sequence is already sorted so there is no inversion count.
+Input: arr[] = [10, 10, 10]
+Output: 0
+Explanation: As all the elements of array are same, so there is no inversion count.*/
 public class inversionCount {
     static int count;
     static void main() {
