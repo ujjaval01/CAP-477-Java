7202ef3b9bebc37335023ab0da0ff8822c51e02f
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-17 09:55:33 +0530

    Update: added the question of this code from the GFG

--- dsaWithJava/src/sortCycle/missingNumberUsingCS.java
+++ dsaWithJava/src/sortCycle/missingNumberUsingCS.java
@@ -1,5 +1,13 @@
 package sortCycle;
-
+/*You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing.
+Your task is to identify and return the missing element.
+Examples:
+Input: arr[] = [1, 2, 3, 5]
+Output: 4
+Explanation: All the numbers from 1 to 5 are present except 4.
+Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+Output: 6
+Explanation: All the numbers from 1 to 8 are present except 6.*/
 public class missingNumberUsingCS {
     static void main() {
         int[] arr = {1,3,4,2,6};
