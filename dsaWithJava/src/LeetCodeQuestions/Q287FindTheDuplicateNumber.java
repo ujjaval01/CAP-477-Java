76498ebf840b3a4ddab9920b1c14bc0bcf563359
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-19 10:17:12 +0530

    Resolve: In this file has issue, if the one element repeat more than 2 time, then he print also all reteated element

--- dsaWithJava/src/LeetCodeQuestions/Q287FindTheDuplicateNumber.java
+++ dsaWithJava/src/LeetCodeQuestions/Q287FindTheDuplicateNumber.java
@@ -1,22 +1,34 @@
 package LeetCodeQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q287FindTheDuplicateNumber {
     static void main() {
-        int[] arr = {3,3,3,3,3,3
-        };
+        int[] arr = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicate(arr));
     }
 
-    private static int findDuplicate(int[] arr) {
+    private static List<Integer> findDuplicate(int[] arr) {
         int i = 0;
+        List<Integer> ans = new ArrayList<>();
         while(i<arr.length){
             if(arr[i] == i+1) i++;
             else {
                 int idx = arr[i] - 1;
-                if(arr[i] == arr[idx]) return arr[i];
-                swap(i, idx, arr);
+                if(arr[i] != arr[idx]){
+                    swap(i, idx, arr);
+                }else{
+                    i++;
+                }
             }
-        }return arr[i];
+        }
+        for(i = 0; i<arr.length; i++){
+            if(arr[i] != i+1){
+                ans.add(arr[i]);
+            }
+        }
+        return ans;
     }
 
     private static void swap(int i, int j, int[] arr) {
