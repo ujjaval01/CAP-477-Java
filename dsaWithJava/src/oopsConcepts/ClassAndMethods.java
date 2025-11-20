edc3374aa4658a4de97ea2d1b227a5206934ac41
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-20 11:58:00 +0530

    Update: Added the some comments for the futures to understand code easily

--- dsaWithJava/src/oopsConcepts/ClassAndMethods.java
+++ dsaWithJava/src/oopsConcepts/ClassAndMethods.java
@@ -6,20 +6,23 @@ class Students{
     private int rollNo;
     String name;
     double cgpa;
-    public void print(){
-        System.out.println(name + " " +rollNo + " " + cgpa);
-    }
+    // using getter setter methods to access private data members
     public int getRollNo(){
         return rollNo;
     }
     public void setRollNo(int x){
         rollNo = x;
     }
+//    print the above data members
+    public void print(){
+        System.out.println(name + " " +rollNo + " " + cgpa);
+    }
 
 }
 public class ClassAndMethods {
     static void main() {
         Scanner sc = new Scanner(System.in);
+//        creating object of the outer class to access the data member inside the inner or main class
         Students s1 = new Students();
 //        s1.rollNo = 9;
 
@@ -29,7 +32,7 @@ public class ClassAndMethods {
         s1.name = "ujvl";
         s1.setRollNo(12);
         System.out.println(s1.getRollNo());
-
+// taking input in stringbuilder
         StringBuilder sb = new StringBuilder(sc.next());
         System.out.println(sb);
     }
