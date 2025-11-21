11fc0a2f2dae8a64bbf722baf5f011e4f9bd6ef1
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-21 10:54:00 +0530

    Update: Add same fraction class in optimized version, taking help from the chatGpt

--- dsaWithJava/src/oopsConcepts/FractionClass.java
+++ dsaWithJava/src/oopsConcepts/FractionClass.java
@@ -41,6 +41,73 @@ class Fraction{
     }
 }
 
+// This solution provided from the side of chatGpt, its very clean and optimized version of the fraction...
+
+//class Fraction {
+//    int num;   // numerator
+//    int den;   // denominator
+//
+//    Fraction(int num, int den) {
+//        if (den == 0) {
+//            throw new IllegalArgumentException("Denominator cannot be zero");
+//        }
+//
+//        // Normalize sign (den always positive)
+//        if (den < 0) {
+//            num = -num;
+//            den = -den;
+//        }
+//
+//        this.num = num;
+//        this.den = den;
+//
+//        simplify();
+//    }
+//
+//    Fraction add(Fraction f) {
+//        int newNum = this.num * f.den + this.den * f.num;
+//        int newDen = this.den * f.den;
+//        return new Fraction(newNum, newDen);
+//    }
+//
+//    Fraction sub(Fraction f) {
+//        int newNum = this.num * f.den - this.den * f.num;
+//        int newDen = this.den * f.den;
+//        return new Fraction(newNum, newDen);
+//    }
+//
+//    Fraction multi(Fraction f) {
+//        int newNum = this.num * f.num;
+//        int newDen = this.den * f.den;
+//        return new Fraction(newNum, newDen);
+//    }
+//
+//    Fraction divide(Fraction f) {
+//        if (f.num == 0) {
+//            throw new IllegalArgumentException("Cannot divide by zero fraction");
+//        }
+//        int newNum = this.num * f.den;
+//        int newDen = this.den * f.num;
+//        return new Fraction(newNum, newDen);
+//    }
+//
+//    private void simplify() {
+//        int g = hcf(Math.abs(num), Math.abs(den));
+//        num /= g;
+//        den /= g;
+//    }
+//
+//    private int hcf(int a, int b) {
+//        if (a == 0) return b;
+//        return hcf(b % a, a);
+//    }
+//
+//    void print() {
+//        System.out.println(num + "/" + den);
+//    }
+//}
+
+
 public class FractionClass {
     static void main() {
         Fraction f1 = new Fraction(2,4);
