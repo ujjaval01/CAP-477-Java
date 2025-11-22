53c2ef0aba57be9db252111b2b930e246c8df139
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-22 17:54:46 +0530

    Update: Printing elements using recursion method...

--- dsaWithJava/src/Collections/LinkedList/simpleLinkedList.java
+++ dsaWithJava/src/Collections/LinkedList/simpleLinkedList.java
@@ -6,8 +6,10 @@ class Node{
     Node(int val){
         this.val = val;
     }
-    Node(){
-
+    public static void displayRec(Node head){
+        if(head == null) return;
+        System.out.print(head.val + " ");
+        displayRec(head.next);
     }
     public static void display(Node head){
         Node temp = head;
@@ -20,17 +22,13 @@ class Node{
 
 public static class SimpleLinkedList {
     static void main() {
-        Node n = new Node();
-        Node a = new Node(10);
-        Node b = new Node(20);
-        Node c = new Node(30);
-        Node d = new Node(40);
-        Node e = new Node(50);
-        a.next = b;
-        b.next = c;
-        c.next = d;
-        d.next = e;
-        display(a);
+        Node a = new Node(10); Node b = new Node(20);
+        Node c = new Node(30); Node d = new Node(40);
+        Node e = new Node(50); Node f = new Node(60);
+        a.next = b; b.next = c; c.next = d;
+        d.next = e; e.next = f;
+//        display(a);
+        displayRec(a);
 
     }
 }
