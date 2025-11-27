9c6db086c49c372b866fec109ed9b26263d8742c
Ujjaval Saini <sainiujvl@gmail.com>
2025-11-27 23:14:37 +0530

    Add: solving leetcode qstn 61, rotate a list by k value

---
+++ dsaWithJava/src/Collections/LinkedList/Q61RotateListByK.java
@@ -0,0 +1,41 @@
+package Collections.LinkedList;
+
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k == 0) return head;
+
+        // 1️⃣ Calculate length and get last node
+        ListNode temp = head;
+        int length = 1;
+        while(temp.next != null){
+            temp = temp.next;
+            length++;
+        }
+
+        // 2️⃣ Make it circular
+        temp.next = head;
+
+        // 3️⃣ Find effective rotation
+        k = k % length;
+        int stepsToNewHead = length - k;
+
+        // 4️⃣ Move `temp` to the new last node
+        while(stepsToNewHead-- > 0){
+            temp = temp.next;
+        }
+
+        // 5️⃣ Break the circle and return new head
+        ListNode newHead = temp.next;
+        temp.next = null;
+
+        return newHead;
+    }
+}
+
+
+public class Q61RotateListByK {
+    static void main() {
+
+    }
+}
