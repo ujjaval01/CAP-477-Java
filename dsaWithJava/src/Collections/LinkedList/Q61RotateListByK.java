aaa67af811373a66597edeab2a35fb94da58116c
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-06 20:37:35 +0530

    Update: Fix: Handle edge case for single node or empty list

--- dsaWithJava/src/Collections/LinkedList/Q61RotateListByK.java
+++ dsaWithJava/src/Collections/LinkedList/Q61RotateListByK.java
@@ -1,39 +1,48 @@
 package Collections.LinkedList;
-class Solution3 {
-    public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0) return head;
-
-        // 1️⃣ Calculate length and get last node
-        ListNode temp = head;
-        int length = 1;
-        while(temp.next != null){
-            temp = temp.next;
-            length++;
-        }
-
-        // 2️⃣ Make it circular
-        temp.next = head;
-
-        // 3️⃣ Find effective rotation
-        k = k % length;
-        int stepsToNewHead = length - k;
-
-        // 4️⃣ Move `temp` to the new last node
-        while(stepsToNewHead-- > 0){
-            temp = temp.next;
-        }
-
-        // 5️⃣ Break the circle and return new head
-        ListNode newHead = temp.next;
-        temp.next = null;
-
-        return newHead;
-    }
-}
-
 
 public class Q61RotateListByK {
+    public static int length(ListNode head){
+        int len = 0;
+        ListNode temp = head;
+        while (temp != null){
+            len++;
+            temp = temp.next;
+        }
+        return len;
+    }
+    public static ListNode rotateList(ListNode head, int k){
+        int n = length(head);
+        k %= n;
+        if(head == null || head.next == null || k == 0) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i = 0; i<k+1; i++){
+            fast = fast.next;
+        }
+        while (fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        ListNode a = slow.next;
+        slow.next = null;
+        ListNode temp = a;
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = head;
+        return a;
+    }
     static void main() {
-
+        ListNode a = new ListNode(10); ListNode b = new ListNode(20);
+        ListNode c = new ListNode(30); ListNode d = new ListNode(40);
+        ListNode e = new ListNode(50);  ListNode f = new ListNode(60);
+        a.next = b; b.next = c; c.next = d;
+        d.next = e; e.next = f;
+        int k = 2;
+        ListNode temp = rotateList(a, k);
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
     }
 }
