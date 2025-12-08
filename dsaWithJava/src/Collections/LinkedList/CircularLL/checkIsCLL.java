21137ad6837ed3c35f00cb62ecc1af322b3e6bad
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-09 03:04:14 +0530

    Check Post: Is circular or not

--- dsaWithJava/src/Collections/LinkedList/CircularLL/checkIsCLL.java
+++ dsaWithJava/src/Collections/LinkedList/CircularLL/checkIsCLL.java
@@ -1,26 +1,26 @@
-//package Collections.LinkedList.CircularLL;
-//
-//public class checkIsCLL {
-//    /* Function to print nodes in a given Circular linked list */
-//    static boolean isCircular(Node head) {
-//        // Your code here
-//        Node temp = head;
-//        while(temp != null){
-//            if(temp.next == head){
-//                return true;
-//            }
-//            temp = temp.next;
-//        }
-//        return false;
-//    }
-//    static void main() {
-//        Node a = new  Node(10);
-//        Node b = new  Node(20);
-//        Node c = new  Node(30);
-//        Node d = new  Node(40);
-//        Node e = new  Node(50);
-//        a.next = b; b.next = c; c.next = d; d.next = e;
-//        e.next = a;
-//        System.out.println(isCircular(a));
-//    }
-//}
+package Collections.LinkedList.CircularLL;
+
+public class checkIsCLL {
+    /* Function to print nodes in a given Circular linked list */
+    static boolean isCircular(Node head) {
+        // Your code here
+        Node temp = head;
+        while(temp != null){
+            if(temp.next == head){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    static void main() {
+        Node a = new  Node(10);
+        Node b = new  Node(20);
+        Node c = new  Node(30);
+        Node d = new  Node(40);
+        Node e = new  Node(50);
+        a.next = b; b.next = c; c.next = d; d.next = e;
+        e.next = a;
+        System.out.println(isCircular(a));
+    }
+}
