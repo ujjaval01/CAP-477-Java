0a059ccdc2583de9d7563c47f9011abc546c4a2d
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-11 01:22:44 +0530

    Add: performing push operation on basic stack

---
+++ dsaWithJava/src/Stack/BasicOpOfStack.java
@@ -0,0 +1,14 @@
+package Stack;
+
+import java.util.Stack;
+
+public class BasicOpOfStack {
+    static void main() {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(13);
+        st.push(40);
+        System.out.println(st);
+    }
+}
