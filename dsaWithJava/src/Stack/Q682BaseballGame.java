d8a41cc5ba3156d85fab1ca15277c73d85b3b0f0
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-13 05:19:39 +0530

    Enhanced with for loop and switches

--- dsaWithJava/src/Stack/Q682BaseballGame.java
+++ dsaWithJava/src/Stack/Q682BaseballGame.java
@@ -4,23 +4,25 @@ import java.util.Stack;
 
 public class Q682BaseballGame {
     static void main() {
-
+        String[] arr = {"5","2","C","D","+"};
+        System.out.println(calPoints(arr));
     }
     public static int calPoints(String[] arr) {
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
-        for(int i = 0; i<n; i++){
-            String s = arr[i];
-            if(s.equals("C")) st.pop();
-            else if(s.equals("D")) st.push(2 * st.peek());
-            else if(s.equals("+")){
-                int top = st.pop();
-                int secondTop = st.peek();
-                int sum = top + secondTop;
-                st.push(top);
-                st.push(sum);
+        for (String s : arr) {
+            switch (s) {
+                case "C" -> st.pop();
+                case "D" -> st.push(2 * st.peek());
+                case "+" -> {
+                    int top = st.pop();
+                    int secondTop = st.peek();
+                    int sum = top + secondTop;
+                    st.push(top);
+                    st.push(sum);
+                }
+                default -> st.push(Integer.parseInt(s));
             }
-            else st.push(Integer.parseInt(s));
         }
         int sum = 0;
         while(!st.isEmpty()){
