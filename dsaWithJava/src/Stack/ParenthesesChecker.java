2094234a9c1306060027738ea89722d25ea21c2b
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-13 02:20:16 +0530

    Writing a code to check the given paramtheses is balanced or not

---
+++ dsaWithJava/src/Stack/ParenthesesChecker.java
@@ -0,0 +1,35 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ParenthesesChecker {
+    static void main() {
+
+    }
+    public static boolean isBalanced(String s) {
+        // code here
+        int n = s.length();
+        if(n%2 == 1) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch  =='{' || ch == '[') st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                else{
+                    char top = st.peek();
+                    if(sameStyle(top, ch)) st.pop();
+                    else return false;
+                }
+            }
+        }
+        return (st.isEmpty());
+    }
+
+    static boolean sameStyle(char a, char b) {
+        if(a == '(' && b == ')') return true;
+        if(a == '[' && b == ']') return true;
+        if(a == '{' && b == '}') return true;
+        return false;
+    }
+}
