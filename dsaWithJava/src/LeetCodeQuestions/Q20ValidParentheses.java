9218903a2e31e773356dfb7551368994a7d5f700
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-13 04:16:12 +0530

    solving leetcode question to check the given parentheses is in valid structured or  not

---
+++ dsaWithJava/src/LeetCodeQuestions/Q20ValidParentheses.java
@@ -0,0 +1,36 @@
+package LeetCodeQuestions;
+
+import java.util.Stack;
+
+public class Q20ValidParentheses {
+    static void main() {
+        String str = "((){}[])";
+        System.out.println(isBalanced(str));
+    }
+    public static boolean isBalanced(String s) {
+        // code here
+        int n = s.length();
+        if(n%2 == 1) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch  =='{' || ch == '[') st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                else{
+                    char top = st.peek();
+                    if(sameStyle(top, ch)) st.pop();
+                    else return false;
+                }
+            }
+        }
+        return (st.isEmpty());
+    }
+
+    static boolean sameStyle(char a, char b) {
+        if(a == '(' && b == ')') return true;
+        if(a == '[' && b == ']') return true;
+        if(a == '{' && b == '}') return true;
+        return false;
+    }
+}
