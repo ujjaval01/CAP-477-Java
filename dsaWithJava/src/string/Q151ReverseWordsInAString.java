02a0340eaddb1e326ba25148d1f038dfed368bb8
Ujjaval Saini <sainiujvl@gmail.com>
2025-12-17 23:39:49 +0530

    solving leetcode string based question to reverse words in th estring

---
+++ dsaWithJava/src/string/Q151ReverseWordsInAString.java
@@ -0,0 +1,22 @@
+package string;
+
+public class Q151ReverseWordsInAString {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
